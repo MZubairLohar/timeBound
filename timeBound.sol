351b1a50e1c4770d5abdef3fe7af138cca27a2c8
MZubairLohar <zubairgituser@gmail.com>
2020-06-12 21:36:07 +0500

    events & transfer types

--- timeBound.sol
+++ timeBound.sol
@@ -1,3 +1,5 @@
+pragma solidity >=0.4.21 <0.7.0;
+
 import "./SafeMaths.sol";
 
 contract timeBound{
@@ -14,7 +16,8 @@ contract timeBound{
     using SafeMaths for uint256;
     
     //mapping
-    mapping(address => uint256) private _balance;
+    mapping(address => uint256) private _balances;
+    mapping(address => mapping(address => uint256)) private _allowances;
     
  
  // constructor 
@@ -27,6 +30,8 @@ contract timeBound{
          _totalSupply <= _cap;
         
     }
+    //events
+    event Transfer (address from, address to, uint256 amount);
     
     
     // view functions  
@@ -47,4 +52,29 @@ contract timeBound{
     function balanceOf(address account) public view returns(uint256){
         return _balance[account];
     }
+    
+    //transfer types 
+    function transfer(address recipient, uint256 amount ) internal returns(bool){
+        _transfer(msg.sender(), recipient, amount);
+        return true;
+    } 
+    function _transfer(address sender, address recipient, uint256 amount)internal {
+        require(msg.sender != address(0) ,"invalid address");
+        require(to != address(0),"invalid address");
+        beforeTokenTransfer(address sender, address to , uint256 amount);
+        _balances[sender] = _balances[sender].sub(amount,"amount should not exceed from balance");
+        _balances[to] = _balances[to].add(amount);
+        emit(sender , to, amount);
+    }
+    function transferFrom()public returns(bool){
+        _transfer(address sender, address to, uint256 amount);
+        _approved(sender, msg.sender(), _allowances[sender][msg.sender()].sub(amount,"amount exceed allowances"));
+        return true;
+    }
+    
+    
+    
+    
+    
+    
 }
